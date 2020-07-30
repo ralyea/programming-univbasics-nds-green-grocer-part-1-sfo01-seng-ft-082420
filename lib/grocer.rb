35ce4784cfca14550adb63e18ff330125600deca
Paul Ralyea <047par@gmail.com>
2020-07-30 03:13:55 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -1,9 +1,21 @@
 def find_item_by_name_in_collection(name, collection)
+ #class Array
+  #def value_included? name
+    each do |collection|
+      return true if collection.has_value? name
+    end
+    nil
+  end
+ 
+ # array.select { |hash| hash['name'].include?('John') }
+  #array.select { |collection| collection['name'].include
+  #?('John') }
+
+  
+  
   # Implement me first!
   #
   # Consult README for inputs and outputs
-
-end
 
 def consolidate_cart(cart)
   # Consult README for inputs and outputs
